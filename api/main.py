8b1205ff8b6b522d636d19754fa1c0deefa83354
M1SKA-DJ <zaicevandrei2006@gmail.com>
2026-09-09 12:11:46 +0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import groups, teachers, rooms, lessons, subjects, replacements, admin
+from api.routers import groups, teachers, rooms, lessons, subjects, replacements, admin
+
 
 app = FastAPI()
 
