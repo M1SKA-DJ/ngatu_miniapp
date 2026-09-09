fd34e0b0e1a99acd917e484efc02fd73c15e954f
M1SKA-DJ <zaicevandrei2006@gmail.com>
2026-09-09 11:39:40 +0300

    Update vite.config.js

--- miniapp/vite.config.js
+++ miniapp/vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
+  root: ".", // ← критично
+  publicDir: "public",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: resolve(__dirname, "public/index.html"), // ← критично
+    },
+  },
 });
